57d6e0acb75633d4a48c768e1f6d16bce7b149ca
Moriyama Masataka <masataka.312@gmail.com>
2021-01-10 16:53:06 +0900

    test

--- srv/index.ts
+++ srv/index.ts
@@ -1,14 +1,43 @@
+const getTasks = require('./api/get');
 let express = require('express');
 let app = express();
+const mysql = require('mysql');
+import {Connection} from 'mysql';
+console.log('hoge');
+// console.log(getFunc);
 
-app.get('/', () => {
-	console.log('hoge');
+// DBへの接続
+interface DatabaseConnectConfig {
+	host: string;
+	user: string;
+	password: string;
+	database: string;
+}
+const DatabaseConnectConfig: DatabaseConnectConfig = {
+	host: 'localhost',
+	user: 'root',
+	password: 'admin',
+	database: 'todolist_db'
+}
+const connection: Connection = mysql.createConnection(DatabaseConnectConfig);
+
+connection.connect((err: any) => {
+	if(err) {
+		console.log('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('success');
+	// console.log(getFunc);
+	getTasks(connection, 0);
 });
-app.get('/api/test', (req: any, res: any) => {
-	console.log(req);
-	console.log('test');
-	res.send({api: 'test'});
-});
+
+// app.get('/', () => {
+// 	console.log('hoge');
+// });
+// app.get('/api/tasks', (req, res) => {
+// 	const status = +req.query.status || 0;
+
+// });
 
 const port = 3001;
 app.listen(port);
