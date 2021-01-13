de788c1c75a05277e959c7e382ade6a6986db970
Moriyama Masataka <masataka.312@gmail.com>
2021-01-13 09:31:38 +0900

    mysql周りを消す

--- srv/index.ts
+++ srv/index.ts
@@ -1,35 +1,10 @@
 const getTasks = require('./api/get');
 let express = require('express');
 let app = express();
-const mysql = require('mysql');
-import {Connection} from 'mysql';
 console.log('hoge');
 // console.log(getFunc);
 
 // DBへの接続
-interface DatabaseConnectConfig {
-	host: string;
-	user: string;
-	password: string;
-	database: string;
-}
-const DatabaseConnectConfig: DatabaseConnectConfig = {
-	host: 'localhost',
-	user: 'root',
-	password: 'admin',
-	database: 'todolist_db'
-}
-const connection: Connection = mysql.createConnection(DatabaseConnectConfig);
-
-connection.connect((err: any) => {
-	if(err) {
-		console.log('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('success');
-	// console.log(getFunc);
-	getTasks(connection, 0);
-});
 
 // app.get('/', () => {
 // 	console.log('hoge');
