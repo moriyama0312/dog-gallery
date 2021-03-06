9b4a13eb1eda5a30511ea27d95af3eaa1890235a
Moriyama Masataka <masataka.312@gmail.com>
2021-03-06 16:51:23 +0900

    make

--- src/tsx/components/Make.tsx
+++ src/tsx/components/Make.tsx
@@ -1,50 +1,51 @@
-import React, { FC, FormEvent } from 'react';
+import React, { FC, FormEvent, ChangeEvent } from 'react';
 import Styles from '../../sass/_inc/_components/_Make.scss';
 import { Style } from '../interfaces/index';
 
 const Style = Styles as Style;
 
 interface MakeProps {
-	SubmitFunc: (e: FormEvent) => void;
+	submitHandler: (e: FormEvent<HTMLFormElement>) => void;
+	changeHandler: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }
 
-const MakeComponent: FC<MakeProps> = ({ SubmitFunc }) => {
+const MakeComponent: FC<MakeProps> = ({ submitHandler, changeHandler }) => {
 	return (
 		<div className={Style.container}>
 			<div className={Style.form}>
 				<form
-					onSubmit={SubmitFunc}
+					onSubmit={submitHandler}
 				>
 					<div className={Style.form__block}>
 						<h3 className={Style.form__block__title}>タスクのタイトル</h3>
-						<input name="task_title" placeholder="タスクタイトルを入力してください"></input>
+						<input name="task_title" placeholder="タスクタイトルを入力してください" onChange={changeHandler}></input>
 					</div>
 					<div className={Style.form__block}>
 						<h3 className={Style.form__block__title}>タスクの詳細</h3>
-						<textarea name="task_detail" placeholder="タスクの詳細を入力してください"></textarea>
+						<textarea name="task_detail" placeholder="タスクの詳細を入力してください" onChange={changeHandler}></textarea>
 					</div>
 					<div className={Style.form__block}>
 						<h3 className={Style.form__block__title}>タスクのカテゴリー</h3>
 						<ul className={Style.checkbox__list}>
-							<li><input id="checkbox__cat__item01" type="checkbox" name="cat_item01"></input><label htmlFor="checkbox__cat__item01">趣味</label></li>
-							<li><input id="checkbox__cat__item02" type="checkbox" name="cat_item02"></input><label htmlFor="checkbox__cat__item02">勉強</label></li>
-							<li><input id="checkbox__cat__item02" type="checkbox" name="cat_item03"></input><label htmlFor="checkbox__cat__item03">仕事</label></li>
-							<li><input id="checkbox__cat__item02" type="checkbox" name="cat_item04"></input><label htmlFor="checkbox__cat__item04">交際</label></li>
+							<li><input id="checkbox__cat__item01" type="radio" value="1" name="cat_item" onChange={changeHandler}></input><label htmlFor="checkbox__cat__item01">趣味</label></li>
+							<li><input id="checkbox__cat__item02" type="radio" value="2" name="cat_item" onChange={changeHandler}></input><label htmlFor="checkbox__cat__item02">仕事</label></li>
+							<li><input id="checkbox__cat__item02" type="radio" value="3" name="cat_item" onChange={changeHandler}></input><label htmlFor="checkbox__cat__item03">勉強</label></li>
+							<li><input id="checkbox__cat__item02" type="radio" value="4" name="cat_item" onChange={changeHandler}></input><label htmlFor="checkbox__cat__item04">交際</label></li>
 						</ul>
 					</div>
 					<div className={Style.form__block}>
 						<h3 className={Style.form__block__title}>タスクの締め切り</h3>
 						<ul className={Style.deadline__list}>
 							<li>
-								<input type="number" name="deadline_year" placeholder="2020"></input>
+								<input type="number" name="deadline_year" placeholder="2021" onChange={changeHandler}></input>
 								年
 							</li>
 							<li>
-								<input type="number" name="deadline_month" placeholder="11"></input>
+								<input type="number" name="deadline_month" placeholder="01" onChange={changeHandler}></input>
 								月
 							</li>
 							<li>
-								<input type="number" name="deadline_day" placeholder="30"></input>
+								<input type="number" name="deadline_day" placeholder="01" onChange={changeHandler}></input>
 								日
 							</li>
 						</ul>
--- src/tsx/containers/Make.tsx
+++ src/tsx/containers/Make.tsx
@@ -1,14 +1,62 @@
-import React, { FC, FormEvent } from 'react';
+import React, { FC, FormEvent, ChangeEvent, useState } from 'react';
 import MakeComponent from '../components/Make';
 
 const MakeContainer: FC = () => {
-	const SubmitFunc = (e: FormEvent) => {
-		e.preventDefault();
-		console.log('hoge');
-		console.log(e);
+	interface FormValue {
+		task_title: string;
+		task_detail: string;
+		task_category: number;
+		task_createdby: string;
+		task_status: number;
+		task_deadline?: Date;
+	}
+	const initialFormValue: FormValue = {
+		task_title: '',
+		task_detail: '',
+		task_category: 0,
+		task_createdby: '',
+		task_status: 1
+	}
+	interface Deadline {
+		[key: string]: string;
+	}
+	const initialDeadline: Deadline = {
+		deadline_year: '2021',
+		deadline_month: '01',
+		deadline_day: '01'
 	};
+	const [formValue, setFormValue] = useState(initialFormValue);
+	const [deadline, setDeadline] = useState(initialDeadline);
 
-	return <MakeComponent SubmitFunc={SubmitFunc} />
+	const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(e.target);
+		console.log(formValue);
+	};
+	const changeHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		const key = e.target.name;
+		const value = e.target.value;
+		let newFormValue: FormValue;
+		if(key.indexOf('deadline') >= 0) {
+			const newDeadline = {...deadline, ...{[key]: value}};
+			let newDeadlineString = '';
+			setDeadline(newDeadline);
+			(Object.keys(newDeadline) as (keyof Deadline)[]).forEach((key) => {
+				if(key === 'deadline_day') {
+					newDeadlineString += newDeadline[key];
+				}else {
+					newDeadlineString += newDeadline[key] + '-';
+				}
+			});
+			newFormValue = {...formValue,...{task_deadline: new Date(newDeadlineString)}};
+			setFormValue(newFormValue);
+		}else {
+			newFormValue = {...formValue,...{[key]: value}};
+			setFormValue(newFormValue);
+		}
+	}
+
+	return <MakeComponent submitHandler={submitHandler} changeHandler={changeHandler} />
 }
 
 export default MakeContainer;
